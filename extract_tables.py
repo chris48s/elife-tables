6bb972bf8479d022ebd2eb90f6cb8814eebc6c09
chris48s <chris.shaw480@gmail.com>
2020-09-03 10:39:13 +0100

    stratify tables by sec-type

--- extract_tables.py
+++ extract_tables.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python3
-
 from pathlib import Path
 from lxml import etree
 from settings import IN_PATH, OUT_PATH
+
 
 for article in list(IN_PATH.rglob('*.xml')):
     print(f'Parsing: {str(article)} ...')
     text = article.read_bytes()
     tree = etree.fromstring(text)
-    tables = tree.xpath('//table-wrap')
-    for i, table in enumerate(tables):
-        subdir = OUT_PATH / Path(str(article.parts[-1]))
-        subdir.mkdir(parents=True, exist_ok=True)
-        outfile = subdir / Path(f'table-{str(i)}.xml')
-        with open(outfile, "wb") as f:
-            f.write(etree.tostring(table))
+    sections = tree.xpath('//sec')
+    section_types = set()
+    for sec in sections:
+        if sec.get('sec-type'):
+            section_types.add(sec.get('sec-type'))
+
+    for sec_type in section_types:
+        tables = tree.xpath(f'//sec[@sec-type="{sec_type}"]//table-wrap')
+        for i, table in enumerate(tables):
+            subdir = OUT_PATH / Path(str(article.parts[-1])) / Path(sec_type)
+            subdir.mkdir(parents=True, exist_ok=True)
+            outfile = subdir / Path(f'table-{str(i)}.xml')
+            with open(outfile, "wb") as f:
+                f.write(etree.tostring(table))
